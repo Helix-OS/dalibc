7af7cdb6e0e7e103c38782ca92f893266b0f4573
User Name <lemrdragons@gmail.com>
2014-06-11 05:08:45 -0500

    Filling in ctype.h

--- include/ctype.h
+++ include/ctype.h
@@ -1,5 +1,20 @@
 #ifndef _CTYPE_H
 #define _CTYPE_H 1
 
+int isalpha( int c );
+int isdigit( int c );
+int isalnum( int c );
+
+int iscntrl( int c );
+int isgraph( int c );
+int islower( int c );
+int isupper( int c );
+int isprint( int c );
+int ispunct( int c );
+int isspace( int c );
+int isxdigit( int c );
+
+int toupper( int c );
+int tolower( int c );
 
 #endif /* _CTYPE_H */
