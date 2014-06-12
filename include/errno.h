72a94be1f20108eaed0c1244b4eb96d0ba836002
User Name <lemrdragons@gmail.com>
2014-06-12 02:29:39 -0500

    Implementing "none" host makefile, fixing bug in errno.h

--- include/errno.h
+++ include/errno.h
@@ -1,7 +1,7 @@
 #ifndef _ERRNO_H
 #define _ERRNO_H 1
 
-#define errno (*_dalibc_get_errno( ));
+#define errno (*_dalibc_get_errno( ))
 
 int *_dalibc_get_errno( );
 void _dalibc_set_errno( int error );
