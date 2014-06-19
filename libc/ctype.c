b7b13bf91af39f51907308b251372c36644e3c6a
My Name <lemrdragons@gmail.com>
2014-06-19 02:05:10 -0500

    Adding more ctype stuff

--- libc/ctype.c
+++ libc/ctype.c
@@ -11,3 +11,52 @@ int islower( int c ){
 int isupper( int c ){
 	return c >= 'A' && c <= 'Z';
 }
+
+int isdigit( int c ){
+	return c >= '0' && c <= '9';
+}
+
+int isalnum( int c ){
+	return isalpha( c ) || isdigit( c );
+}
+
+int isspace( int c ){
+	return  c == ' '  || c == '\n' || c == '\r' || c == '\t' ||
+		c == '\v' || c == '\f';
+}
+
+int isprint( int c ){
+	return c >= ' ' && c != 0x7f;
+}
+
+int ispunct( int c ){
+	return isprint( c ) && !isalnum( c ) && !isspace( c );
+}
+
+int isgraph( int c ){
+	return isprint( c ) && !isspace( c );
+}
+
+int isxdigit( int c ){
+	return isdigit( c ) ||
+		( c >= 'a' && c <= 'f' ) ||
+		( c >= 'A' && c <= 'F' );
+}
+
+int toupper( int c ){
+	int ret = c;
+
+	if ( islower( c ))
+		ret = c + 0x20;
+
+	return ret;
+}
+
+int tolower( int c ){
+	int ret = c;
+
+	if ( isupper( c ))
+		ret = c - 0x20;
+
+	return ret;
+}
